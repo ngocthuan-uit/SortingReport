c5b063794a987b8c2c2b37c812f00344265825d7
ngocthuan-uit <25521819@gm.uit.edu.vn>
2026-02-13 15:00:47 +0700

    OK

--- DSA/SORT/bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(a):
-    n=len(a)
-    for i in range(n):
-        for j in range(n-i-1):
-            if (a[j]>a[j+1]):
-                a[j], a[j+1]= a[j+1],a[j]
-a=[9,8,1,4,3,2,6,5,7,0]
-bubble_sort(a)
-print(a)
--- OOP/oop.py
+++
@@ -1,7 +0,0 @@
-class Foo:
-    __name="Foo"
-    def __getname(self):
-        print(self.__name)
-    def get(self):
-        self.__getname()
-Foo().get()
